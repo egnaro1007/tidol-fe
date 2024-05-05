739def4ff006d88d6f3d907305ad12ca785c7b33
egnaro1007 <ndviettuan@gmail.com>
2024-05-05 14:46:58 +0700

    Config rewrite api url

--- next.config.js
+++ next.config.js
@@ -1,11 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  trailingSlash: true,
   rewrites() {
     return [
       {
         source: '/api/:path*',
-        destination: `http://localhost:3000/api/:path*`,
+        destination: `http://localhost:8000/api/:path*/`,
       }
     ]
   }
